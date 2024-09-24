4a1861a022b076bf00f6a2a9af8504040ad55355
Irving <dev.lemirving@gmail.com>
2024-09-24 14:24:38 -0500

    gemini ajeitou meu codigo

--- codigo-aula-git.c
+++ codigo-aula-git.c
@@ -2,25 +2,37 @@
 #include <stdlib.h>
 #include <time.h>
 
-typedef struct{
+typedef struct {
     char nome[20];
-}ListaNomes;
+} ListaNomes;
 
-int main(int argc, char const *argv[]){
-    
-    ListaNomes variosNomes[20];
+int main(int argc, char const *argv[]) {
+    ListaNomes variosNomes[10];
+    srand(time(NULL));
+    int i = rand() % 10; // Índice aleatório entre 0 e 9
 
-    FILE *nomes = fopen("nomes.txt","r++");
-    if(nomes == NULL)
-        exit;
+    FILE *nomes = fopen("nomes.txt", "r");
+    if (nomes == NULL) {
+        perror("Erro ao abrir o arquivo");
+        exit(EXIT_FAILURE);
+    }
 
-    
+    int numNomes = 0;
+    while (fscanf(nomes, "%s", variosNomes[numNomes].nome) != EOF) {
+        numNomes++;
+    }
+
+    fclose(nomes);
+
+    if (numNomes == 0) {
+        printf("Nenhum nome encontrado no arquivo.\n");
+        return 1;
+    }
 
     printf("Essa é a aula de git\n");
-    printf("Sortearemos um noem aleatorio: \n");
+    printf("Sortearemos um nome aleatorio: \n");
 
-
-    // printf("Nome: %s",nome);
+    printf("Nome: %s\n\n", variosNomes[i].nome);
 
     return 0;
 }
