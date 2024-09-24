01195a72928ebbe2db24986ad1717e43ec068d31
Irving <dev.lemirving@gmail.com>
2024-09-24 14:01:00 -0500

    Consertando bug tal

---
+++ codigo-aula-git.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char const *argv[]){
+    
+    char nome[20];
+
+    FILE *nomes = fopen("nomes.txt","r++");
+    if(nomes == NULL)
+        exit;
+
+    
+
+    printf("Essa é a aula de git\n");
+    printf("Sortearemos um noem aleatorio: \n");
+
+
+    printf("Nome: %s",nome);
+
+    return 0;
+}
